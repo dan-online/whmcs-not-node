6a5a6700f792f4ebce7a492623120819d9035879
DanCodes <dan@dancodes.online>
2024-05-20 10:48:31 +0100

    fix: set content type

--- src/module/base.ts
+++ src/module/base.ts
@@ -3,21 +3,36 @@ import { WhmcsApi } from "..";
 
 export abstract class BaseModule {
   async request(methodName: string, options?: any): Promise<any> {
+    options.identifier = WhmcsApi.options.identifier;
+    options.secret = WhmcsApi.options.secret;
+    options.action = methodName;
+    options.responsetype = "json";
 
     const searchParams = new URLSearchParams();
-    searchParams.set("identifier", WhmcsApi.options.identifier);
-    searchParams.set("secret", WhmcsApi.options.secret);
-    searchParams.set("action", methodName);
-    searchParams.set("responsetype", "json");
+    
+    for (const key in options) {
+      searchParams.append(key, options[key]);
+    }
 
     return new Promise(async (resolve, reject) => {
       try {
         const res = await ky(WhmcsApi.options.apiUrl, {
           method: "post",
           body: searchParams,
+          throwHttpErrors: false,
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          }
         })
 
-        const data = await res.json() as any;
+        let data;
+
+        if (res.headers.get("content-type")?.includes("application/json")) {
+          data = await res.json();
+        } else {
+          data = await res.text();
+        }
+          
 
         if (data.result != "success") return reject(data);
         resolve(data);
